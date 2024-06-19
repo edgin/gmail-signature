d6f941ce9b462213ef47c4ecebf5d537d3dfa833
edgin <lulemat@gmail.com>
2024-06-19 09:44:15 +0200

    first commit

---
+++ form_express.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express() 
+const port = process.env.PORT || 1024
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const handlebars = require('express-handlebars');
+
+app.use(bodyParser({extended: false}))
+
+app.use(express.json())
+app.use(cors());
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+
+app.get( '/', ( req, res ) => res.render( 'form' ) )
+app.get( '/thank-you', ( req, res ) => res.status( 303 ).render( 'thank-you' ) )
+
+const client = require('@jsreport/nodejs-client')('http://localhost:5488');
+
+app.post( '/form_post',  ( req, res , next) => {
+    client
+    .render({
+      template: {
+        shortid:'8dcpXx2j6',
+        recipe: 'html',
+        engine: 'handlebars'
+      },
+      data: JSON.parse(JSON.stringify(req.body))})
+    .then(response => response.pipe(res))
+    .then(console.log('test', req.body))
+    .catch(next)
+})
+
+app.post( '/form_post_test', ( req, res ) => {
+    console.log(JSON.parse(JSON.stringify(req.body)))
+    res.redirect( 303, 'thank-you' )})
+
+app.use( ( req, res ) => res.status( 404 ).render( '404' ) )
+app.use( ( err, req, res, next ) => res.status( 500 ).render( 500 ) )
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://127.0.0.1:${port}`)
+  })
